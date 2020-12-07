3c63397b3ef7a2b0db32cfea2f88b18b6f16e6c5
Yulia Mironova <mironova@bk.ru>
2020-12-07 11:57:53 +0300

    add genimotion

--- src/test/java/ClockTest.java
+++ src/test/java/ClockTest.java
@@ -1,4 +1,5 @@
 import drivers.AndroidStudioEmu;
+import drivers.GenimotionEmu;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import pages.ClockPage;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -18,24 +20,28 @@ public class ClockTest {
     private AppiumDriver driver;
     private URL serverUrl;
     private DesiredCapabilities capabilities;
+    private ClockPage clockPage1;
 
 
     @BeforeEach
     public void createDriver() throws MalformedURLException {
-        driver = AndroidStudioEmu.setASEmu();
+       // driver = AndroidStudioEmu.setASEmu();
+        driver = GenimotionEmu.setGenEmu();
 
     }
 
     @Test
     public void calApp() throws MalformedURLException {
 
+        clockPage1 = new ClockPage(driver);
+        clockPage1.someClick();
 
-        WebDriverWait wait = new WebDriverWait(driver, 10);
+        /*WebDriverWait wait = new WebDriverWait(driver, 10);
         driver.findElement(By.xpath("//android.widget.TextView[@text='Alarm']")).click();
         driver.findElement(By.xpath("//android.widget.TextView[@text='Clock']")).click();
         driver.findElement(By.xpath("//android.widget.TextView[@text='Timer']")).click();
         driver.findElement(By.xpath("//android.widget.TextView[@text='Stopwatch']")).click();
-
+*/
 
         log.info("5. quit");
 
