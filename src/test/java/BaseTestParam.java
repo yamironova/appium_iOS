29a04850aea01267bf9cfefd8925a220367cd517
Yulia Mironova <mironova@bk.ru>
2020-12-21 10:57:43 +0300

    parametrized from file

--- src/test/java/BaseTestParam.java
+++ src/test/java/BaseTestParam.java
@@ -3,23 +3,13 @@ import drivers.GenimotionEmu;
 import drivers.WiFiReal;
 import io.appium.java_client.AppiumDriver;
 import lombok.extern.log4j.Log4j2;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.ClockPage;
-import pages.ClockPageASEmu;
-import pages.ClockPageGeni;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 @Log4j2
@@ -30,7 +20,8 @@ public class BaseTestParam {
     private ClockPage clockPage1;
 
     @ParameterizedTest
-    @ValueSource(strings = {"Emu", "Studio", "Real"})
+   // @ValueSource(strings = {"Emu", "Studio", "Real"})
+    @CsvFileSource(resources = "/data.csv", numLinesToSkip = 0)
     public void runClock(String device) throws IOException {
         if (device.equals("Studio")) {
             driver = AndroidStudioEmu.setASEmu();;
